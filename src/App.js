50cbba28a14d60d4fab357a3ab6096f50348d917
Myles Green <SoftMachine@Soft-Machine.local>
2018-08-19 21:48:09 -0400

    added house components and styling

--- src/App.js
+++ src/App.js
@@ -1,68 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import * as actions from './actions';
+import Houses from './components/houses';
 
 class App extends Component {
-  state = {
-    isLoading: true
-  }
   render() {
     return (
       <div>
-        {this.props.houses.map(
-          (house) => (
-            <div key= {house.url}>
-              {house.name}
-              {house.swornMembers.map(member => <div>{member.name}</div>)}
-            </div>
-          ) 
-        )}
+        <div className= "App-header">
+          The Citadel
+        </div>
+        <input/> <button className='search'>search</button>
+        <Houses/>
       </div>
     );
   }
-  
-  componentDidMount() {
-    this.loadHouses()
-  }
-  
-  async loadHouses() {
-   await this.props.fetchHouses();
-  }
 }
 
-function mapStateToProps({houses}) {
-  return {
-    houses
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchHouses: () => dispatch(fetchHouses)
-  }
-}
-
-async function fetchHouses(dispatch) {
-  let response = await fetch('https://www.anapioficeandfire.com/api/houses');
-  let houses = await response.json();
-  let memberRef = []
-  let swornMembers = [];
-  houses.forEach(house => {
-    let currentHouse = {...house,swornMembers: []};
-    house.swornMembers.forEach(swornMember => {
-      currentHouse.swornMembers.push(fetchCharacter(swornMember))
-    })
-    memberRef.push(currentHouse);
-  });
-  dispatch(actions.populateHouses(memberRef));
-  dispatch(actions.populateCharacters(swornMembers));
-}
-
-async function fetchCharacter(url) {
-  let response = await fetch(url);
-  let character = await response.json();
-  return character;
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default App;
---
+++ src/components/houses.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Houses extends Component {
+  state = {
+    isLoading: true
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className= 'houses'>
+        <div className= 'houseTitleBar'>
+          <p className= 'houseTitle'>Houses</p>
+        </div>
+        <div className= 'houseText'>
+          {this.props.houses.map(
+            (house) => (
+              <div key= {house.url}>
+                {house.name}
+                {house.swornMembers.map(member => <div key= {member.url} >{member.name}</div>)}
+              </div>
+            ) 
+          )}
+        </div>
+      </div>
+    </div>
+    );
+  }
+  
+  componentDidMount() {
+    this.loadHouses()
+  }
+  
+  async loadHouses() {
+   await this.props.fetchHouses();
+  }
+}
+
+function mapStateToProps({houses}) {
+  return {
+    houses
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchHouses: () => dispatch(fetchHouses)
+  }
+}
+
+async function fetchHouses(dispatch) {
+  let response = await fetch('https://www.anapioficeandfire.com/api/houses');
+  let houses = await response.json();
+  let memberRef = []
+  let swornMembers = [];
+  houses.forEach(house => {
+    let currentHouse = {...house,swornMembers: []};
+    house.swornMembers.forEach(swornMember => {
+      currentHouse.swornMembers.push(fetchCharacter(swornMember))
+    })
+    memberRef.push(currentHouse);
+  });
+  dispatch(actions.populateHouses(memberRef));
+  dispatch(actions.populateCharacters(swornMembers));
+}
+
+async function fetchCharacter(url) {
+  let response = await fetch(url);
+  let character = await response.json();
+  return character;
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Houses);
